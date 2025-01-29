3a4902b154317bbdbc9989ce17307c5f3d319fa6
KrushnakantPavesoft <krushnakantshindepavesoft@gmail.com>
2025-01-29 11:14:44 +0530

    setup initial project.

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import "@mantine/core/styles.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { createTheme, MantineProvider } from "@mantine/core";
+import NavBar from "./components/Layout/NavBar";
+import LoginPage from "./pages/Login/LoginPage";
+import CartPage from "./pages/Cart/CartPage";
+import Products from "./pages/Products/Products";
+import SignupPage from "./pages/SignUp/SignUp";
+const App = () => {
+  const theme = createTheme({
+    defaultRadius: "sm",
+    fontFamily: "PublicSans, sans-serif",
+    primaryColor: "blue",
+  });
+  return (
+    <MantineProvider theme={theme}>
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  );
+};
+
+export default App;
